024885c24e5776b771276dd114b90a9c3eeafe02
unknown <attaway.code@gmail.com>
2023-12-21 18:28:49 -0800

    adds a database

---
+++ db/seeds.sql
@@ -0,0 +1,19 @@
+INSERT INTO department (name)
+VALUES ("Web Development"),
+       ("Data Science"),
+       ("Math"),
+       ("Electives");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("cook", 45231.12, 1 ),
+       ("robot", 44557.5, 2),
+       ("teacher", 65789.1, 3),
+       ("sales", 35123.12, 4);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("jim","cook", 1),
+       ("john","smith", 3),
+       ("Horus","Lupurcal", 4),
+       ("Benny","Han", 2);
+
+       
