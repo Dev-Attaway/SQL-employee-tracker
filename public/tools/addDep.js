b9b7bf73cf654b98d0ca35b4a0717eeee86cbf85
Nick <145059598+Dev-Attaway@users.noreply.github.com>
2024-01-06 14:11:49 -0800

    Update addDep.js
    Updated spelling mistakes

--- public/tools/addDep.js
+++ public/tools/addDep.js
@@ -2,9 +2,9 @@
 function addDepartment() {
 
     // grabbing the script from dbLink and cli
-    // dbLink.js establishes the connection to the SQL db
-    // We will require the inquirer package to ask sub-queries winthin the main query of 'Add Department'
-    // We will also require the showDepartments functions to show to the console the our new department was apended
+    // dbLink.js establishes the connection to the SQL DB
+    // We will require the inquirer package to ask sub-queries within the main query of 'Add Department'
+    // We will also require the showDepartments functions to show to the console that our new department was appended
     const db = require('../../db/dbLink.js');
     const inquirer = require('inquirer');
     const { showDepartments } = require('../deparments.js');
@@ -19,7 +19,7 @@ function addDepartment() {
             name: 'department',
             message: 'What is the name of the department?',
 
-            // determines if the answer provided by user is truthy 
+            // determines if the answer provided by the user is truthy 
             validate: departmentInput => {
                 if (departmentInput) {
                     return true;
@@ -35,7 +35,7 @@ function addDepartment() {
         .then((answers) => {
 
             // INSERT the data of answers.department into the variable [name VARCHAR(30)] 
-            // this new item who was created using the 'TABLE department' found in db\schema.sql
+            //This new item who was created using the 'TABLE department' found in db\schema.sql
             // is appended into the db
             let sqlCommand = 'INSERT INTO department (name) VALUES (?)'
             db.query(sqlCommand, answers.department, (err) => {
