0fe0018c087b3760a3eba6dda4f1fcc3c6dd5b6a
Nick <145059598+Dev-Attaway@users.noreply.github.com>
2024-01-06 14:13:51 -0800

    Update addEmp.js
    Updated spelling mistakes

--- public/tools/addEmp.js
+++ public/tools/addEmp.js
@@ -1,9 +1,9 @@
 function addEmployee() {
 
     // grabbing the script from dbLink and cli
-    // dbLink.js establishes the connection to the SQL db
-    // We will require the inquirer package to ask sub-queries winthin the main query of 'Add employees'
-    // We will also require the showEmployees functions to show to the console the our new employees was apended
+    // dbLink.js establishes the connection to the SQL DB
+    // We will require the inquirer package to ask sub-queries within the main query of 'Add employees'
+    // We will also require the showEmployees functions to show to the console that our new employees were appended
     const db = require('../../db/dbLink.js');
     const inquirer = require('inquirer');
     const { showEmployees } = require('../employees.js');
@@ -11,9 +11,9 @@ function addEmployee() {
     db.connect(err => {
         if (err) throw err;
 
-        // To grab all the equired data from the two tables, we have to approach this via a nested query
-        // Due to both tables which have variables which are equal in name id (employee) = id (role)
-        // the result of the query hold render 'meshed' data from both tables making it useless for evalutation
+        // To grab all the required data from the two tables, we have to approach this via a nested query
+        // Due to both tables which have variables that are equal in name id (employee) = id (role)
+        //The result of the query hold renders 'meshed' data from both tables making it useless for evaluation
         db.query(`SELECT * FROM employee`, (err, employees) => {
             if (err) throw err;
             db.query(`SELECT * FROM role`, (err, roles) => {
@@ -23,10 +23,10 @@ function addEmployee() {
                         // Adding Employee First Name
                         type: 'input',
                         name: 'firstName',
-                        message: 'What is the employees first name?',
+                        message: "What is the employee's first name?",
                         validate: firstNameInput => {
 
-                            // determines if the answer provided by user is truthy                            
+                            // determines if the answer provided by the user is truthy                            
                             if (firstNameInput) {
                                 return true;
                             } else {
@@ -40,7 +40,7 @@ function addEmployee() {
                     {
                         type: 'input',
                         name: 'lastName',
-                        message: 'What is the employees last name?',
+                        message: "What is the employee's last name?",
                         validate: lastNameInput => {
                             if (lastNameInput) {
                                 return true;
@@ -54,12 +54,12 @@ function addEmployee() {
                         // Adding Employee Role
                         type: 'list',
                         name: 'role',
-                        message: 'What is the employee's role?',
+                        message: "What is the employee's role?",
                         choices: () => {
                             var array = [];
                             for (var i = 0; i < roles.length; i++) {
 
-                                // traverses and pushes the roles.title varaibles onto the array
+                                // traverses and pushes the roles.title variables onto the array
                                 // which is displayed to user as a sub-query of possible roles to choose from 
                                 array.push(roles[i].title);
                             }
@@ -76,8 +76,8 @@ function addEmployee() {
                             array.push('None');
                             for (var i = 0; i < employees.length; i++) {
 
-                                // traverses and pushes the roles.title varaibles onto the array
-                                // which is displayed to user as a sub-query of possible managers to choose from 
+                                // traverses and pushes the roles.title variables onto the array
+                                // which is displayed to the user as a sub-query of possible managers to choose from 
                                 array.push(employees[i].first_name + " " + employees[i].last_name);
                             }
                             var newArray = [...new Set(array)];
@@ -86,7 +86,7 @@ function addEmployee() {
                     }
                 ]).then((answers) => {
 
-                    // Comparing the employees, roles, and storing it into the managerName and roleID
+                    // Comparing the employees and roles, and storing it into the managerName and roleID
                     for (var i = 0; i < employees.length; i++) {
                         let managerName = employees[i].first_name + " " + employees[i].last_name;
                         if (managerName == answers.manager) {
